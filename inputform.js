f911f22157e31dca0d61a04d6c4d9e327120e490
Ashok Mane <ashokmane81800@gmail.com>
2021-06-04 17:44:40 +0530

    [ashok] addded salry range

--- inputform.js
+++ inputform.js
@@ -51,3 +51,10 @@
             //print error message if regex test is failed
                 pwdError.textContent = "Minimum 8 characters";                  
         });
+        
+        const salary = document.querySelector('#salary');
+        const output = document.querySelector('.salary-output');
+        output.textContent = salary.value;
+        salary.addEventListener('input', function(){
+            output.textContent = salary.value;
+        });
